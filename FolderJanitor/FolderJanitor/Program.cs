fd38a8cd0ab6f00dcd5aa06ba2db89de0a0e8630
Dylan-Dotti <invocation42@yahoo.com>
2021-11-05 18:35:04 -0400

    Added TopshelfBoilerplate library

--- FolderJanitor/FolderJanitor/Program.cs
+++ FolderJanitor/FolderJanitor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FolderJanitor
@@ -10,6 +11,7 @@ namespace FolderJanitor
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
